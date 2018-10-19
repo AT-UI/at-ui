2bdd755cfb301c4cd1556fbc1d114480552fe16c
Efe Sözen <efe.sozen@arvato.com>
2018-10-19 14:45:21 +0300

    add turkish language support

---
+++ src/locale/lang/tr-TR.js
@@ -0,0 +1,27 @@
+export default {
+  at: {
+    select: {
+      placeholder: 'Seçiniz',
+      notFoundText: 'Eşleşen kayıt bulunamadı.'
+    },
+    modal: {
+      okText: 'Tamam',
+      cancelText: 'Vazgeç'
+    },
+    pagination: {
+      prevText: 'Önceki',
+      nextText: 'Sonraki',
+      total: 'Toplam',
+      item: 'eleman',
+      items: 'elemanlar',
+      pageSize: '/ sayfa',
+      goto: 'Git',
+      pageText: '',
+      prev5Text: 'Önceki 5 Sayfa',
+      next5Text: 'Sonraki 5 Sayfa'
+    },
+    table: {
+      emptyText: 'Kayıt bulunamadı.'
+    }
+  }
+}
