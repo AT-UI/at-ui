9dbad0fd7945f39e2acfdfe4a4f4cc76b181fa86
Abrahan Silverio <abrahansmg@gmail.com>
2019-06-06 17:45:42 -0400

    added spanish localization js file

---
+++ src/locale/lang/es-ES.js
@@ -0,0 +1,27 @@
+export default {
+  at: {
+    select: {
+      placeholder: 'Seleccionar',
+      notFoundText: 'No encontrado'
+    },
+    modal: {
+      okText: 'Aceptar',
+      cancelText: 'Cancelar'
+    },
+    pagination: {
+      prevText: 'Página Anterior',
+      nextText: 'Página Siguiente',
+      total: 'Total',
+      item: 'elemento',
+      items: 'elementos',
+      pageSize: '/ página',
+      goto: 'Ir a',
+      pageText: '',
+      prev5Text: '5 Páginas Atrás',
+      next5Text: '5 Páginas Adelante'
+    },
+    table: {
+      emptyText: 'No hay datos'
+    }
+  }
+}
