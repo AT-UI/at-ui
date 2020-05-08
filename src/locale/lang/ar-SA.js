3642cb0a6d3c8f513387e8e12f01f080f3502def
Faisal K <kwmx@users.noreply.github.com>
2020-05-08 03:40:08 +0300

    Create ar-SA.js
    Arabic config

---
+++ src/locale/lang/ar-SA.js
@@ -0,0 +1,27 @@
+export default {
+  at: {
+    select: {
+      placeholder: 'إختيار',
+      notFoundText: 'لا يوجد بيانات مطابقة'
+    },
+    modal: {
+      okText: 'حسناً',
+      cancelText: 'إلغاء'
+    },
+    pagination: {
+      prevText: 'الصفحه السابقة',
+      nextText: 'الصفحة التالية',
+      total: 'المجموع',
+      item: 'مادة',
+      items: 'المواد',
+      pageSize: '/ صفحة',
+      goto: 'توجة إلى',
+      pageText: '',
+      prev5Text: 'الصفحات الخمس السابقة',
+      next5Text: 'الصفحات الخمس التالية'
+    },
+    table: {
+      emptyText: 'لا يوجد بيانات'
+    }
+  }
+}
